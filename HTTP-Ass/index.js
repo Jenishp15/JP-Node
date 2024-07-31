6d1cf31aa997f6fc59c358c594e4c95b60664386
Jenishp15 <jenishpatel1816@gmail.com>
2024-07-31 10:58:51 +0530

    upadted

--- HTTP-Ass/index.js
+++ HTTP-Ass/index.js
@@ -1,35 +1,35 @@
 const http = require("http")
 const fs = require("fs")
 
-const server = http.createServer((req,res)=>{
-    if(req.url=="/" || req.url=="/home"){
+const server = http.createServer((req, res) => {
+    if (req.url == "/" || req.url == "/home") {
         res.end("Welcome to Home Page")
-    }else if(req.url=="/about"){
+    } else if (req.url == "/about") {
         res.end("Welcome to About Page")
-    }else if(req.url=="/getproductdata"){
-        fs.readFile("./db.json","utf8",(err,data)=>{
-            if(err){
+    } else if (req.url == "/getproductdata") {
+        fs.readFile("./db.json", "utf8", (err, data) => {
+            if (err) {
                 res.end("data not found")
-            }else{
-                const db=JSON.parse(data)
+            } else {
+                const db = JSON.parse(data)
                 res.end(JSON.stringify(db.products))
             }
         })
-    }else if(req.url=="/user"){
-        fs.readFile("./db.json","utf8",(err,data)=>{
-            if(err){
+    } else if (req.url == "/user") {
+        fs.readFile("./db.json", "utf8", (err, data) => {
+            if (err) {
                 res.end("data not found")
-            }else{
-                const db=JSON.parse(data)
+            } else {
+                const db = JSON.parse(data)
                 res.end(JSON.stringify(db.user))
             }
         })
     }
-    else{
+    else {
         res.end("something want wrong")
     }
 })
 
-server.listen(8080,()=>{
+server.listen(8080, () => {
     console.log("server running");
 })
